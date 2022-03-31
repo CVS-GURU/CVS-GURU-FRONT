6bdc6c24ce598ab3ad63aef170ee6e8617da8b02
SeoYeonKim <westkitedev@gmail.com>
2022-03-31 18:01:22 +0900

    🔥 [FEAT] : appbar 스크롤 시 display, hide 구현

--- components/base/Appbar.tsx
+++ components/base/Appbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
@@ -8,21 +8,29 @@ import iconMap from 'lib/iconMap';
 import SearchInput from 'components/common/SearchInput';
 import useWindowSize, { Size } from 'hooks/useWindowSize';
 
+type StyledAppbarWrapper = {
+  isScrollDown: boolean;
+};
+
 const St = {
-  AppbarWrapper: styled.div`
+  AppbarWrapper: styled.div<StyledAppbarWrapper>`
     z-index: 1;
     width: 100%;
     margin: 0 auto;
-
+    background: ${(props) => props.theme.colors.primary};
     height: 80px;
     min-height: 40px;
     display: flex;
-    position: relative;
+    position: fixed;
     justify-content: space-between;
     align-items: center;
     box-shadow: 1px;
-
     box-shadow: 0 -2px 12px 0 rgb(0 0 0 / 6%), 0 -1px 4px 0 rgb(0 0 0 / 18%);
+    transform: ${(props) =>
+      props.isScrollDown
+        ? 'translate3d(0, -100%, 0)'
+        : 'translate3d(0, 0, 0) '};
+    transition: 0.5s;
   `,
 
   HeaderContainerLeft: styled.div``,
@@ -37,6 +45,12 @@ const St = {
     img {
       height: 100%;
       width: 100%;
+    }
+    @media (max-width: 420px) {
+      min-width: 100px;
+      h1 {
+        font-size: 1.5rem;
+      }
     }
   `,
   HeaderContaineCenter: styled.div``,
@@ -84,21 +98,66 @@ const St = {
   `,
 };
 
+const delta = 2;
+let lastScrollTop = 0;
+let fixBox;
+let fixBoxHeight = 0;
+const isServer = typeof window === 'undefined';
 const Appbar = () => {
   const dispatch = useDispatch();
   const router = useRouter();
   const { isMobileSize } = useWindowSize();
-
+  const [isScrollDown, setIsScrollDown] = useState(false);
   const links = [{ name: 'Search', path: '/search' }];
-
+  const didScroll = useRef(false);
   const handleOpenMobileMenu = () => {
     dispatch(commonActions.setIsMobileMenuListOpen(null));
   };
 
   const isMainPage = router.pathname === '/';
 
+  const onScroll = () => {
+    didScroll.current = true;
+  };
+  const hasScrolled = () => {
+    if (isServer) {
+      return;
+    }
+    const nowScrollTop = window.scrollY;
+
+    if (Math.abs(lastScrollTop - nowScrollTop) <= delta) {
+      return;
+    }
+    if (nowScrollTop > lastScrollTop && nowScrollTop > fixBoxHeight) {
+      //Scroll down
+      console.log('scroll down');
+      setIsScrollDown(true);
+    } else {
+      console.log('scroll up');
+      setIsScrollDown(false);
+    }
+    lastScrollTop = nowScrollTop;
+  };
+
+  useEffect(() => {
+    fixBox = document.querySelector('#search-app-bar') as HTMLElement;
+    if (fixBox) {
+      fixBoxHeight = fixBox.offsetHeight;
+    }
+    window.addEventListener('scroll', onScroll);
+    setInterval(() => {
+      if (didScroll.current) {
+        hasScrolled();
+        didScroll.current = false;
+      }
+    }, 250);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [router.query]);
+
   return (
-    <St.AppbarWrapper>
+    <St.AppbarWrapper id="search-app-bar" isScrollDown={isScrollDown}>
       <St.HeaderContainerLeft className="header-container-left flex">
         <Link href={'/'}>
           <St.HeaderContainerLogoWrapper className="flex-center">
